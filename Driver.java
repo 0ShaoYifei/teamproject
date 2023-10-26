9ef1bfe6680c18c17a0734d0fe6a38ae687d7047
ShaoYifei <147827251+0ShaoYifei@users.noreply.github.com>
2023-10-26 20:12:30 +0800

    3
    3

--- Driver.java
+++ Driver.java
@@ -1,2 +1,10 @@
 public class Driver {
+    public static void main(String[] args){
+        System.out.println("""
+                goood
+                goooood
+                god
+                
+                """);
+    }
 }
