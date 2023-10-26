fe9b6a070b1a4b18d0b44f060b09ee4f3e5c45a8
ShaoYifei <147827251+0ShaoYifei@users.noreply.github.com>
2023-10-26 20:41:14 +0800

    5

--- gun.java
+++ gun.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class gun {
+    private Scanner input=new Scanner(System.in);
     private String name;
     private double price;
 
@@ -21,6 +24,12 @@ public class gun {
     public gun() {
     }
 
-    public gun
+    private int mainMenu(){
+        System.out.print("""
+                
+                """);
+        int option=input.nextInt();
+        return option;
+    }
 
 }
