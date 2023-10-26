1496015819380f8188d7ebf64402be9160002587
ShaoYifei <147827251+0ShaoYifei@users.noreply.github.com>
2023-10-26 20:31:26 +0800

    4

--- Driver.java
+++ Driver.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Driver {
+    private Scanner input=new Scanner(System.in);
+
     public static void main(String[] args){
         System.out.println("""
                 goood
---
+++ gun.java
@@ -0,0 +1,26 @@
+public class gun {
+    private String name;
+    private double price;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public gun() {
+    }
+
+    public gun
+
+}
