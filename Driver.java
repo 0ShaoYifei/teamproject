05bc73dce1d6b5086f3d976eb14f718165e6d899
ShaoYifei <147827251+0ShaoYifei@users.noreply.github.com>
2023-10-27 19:53:08 +0800

    11

--- Driver.java
+++ Driver.java
@@ -6,18 +6,12 @@ public class Driver {
                __人人人人人人人人人人人人人人人__
                >BUY THINGS TO KILL OTHERS<
                __Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y^Y__
+               
                 """);
         int option=input.nextInt();
         return option;
-
-
-
-
-
-
-        System.out.println("KILL ALL IN THE WORLD!!!PLEASE!!!KILL YOU!!!");
-        System.exit();
     }
+    
     public void runMenu(){
         int option=mainMenu();
         while (option!=0){
@@ -27,6 +21,24 @@ public class Driver {
             }
         }
     }
+    private void addguns(){
+        input.nextLine();  //dummy read of String to clear the buffer - bug in Scanner class.
+
+        System.out.print("Enter the Gun Name:  ");
+        String name = input.nextLine();
+        System.out.print("Enter the Price:  ");
+        double price = input.nextDouble();
+
+        Yoke temp = new Yoke(yokeName, yokePrice);
+        boolean isAdded = shop.add(temp);
+        if (isAdded){
+            System.out.println("Yoke Added Successfully");
+        }
+        else{
+            System.out.println("No Yoke Added");
+        }
+    }
+
 
     private Scanner input=new Scanner(System.in);
 
